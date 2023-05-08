c6967b2f92ac8b86c8b1de97a8ab671340a11505
PVSaiHarshith <vp82@njit.edu>
2023-05-08 12:22:21 -0400

    implemented cookies in place of localstorage.

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 // pages/_app.js
 import '../styles/global.css'
-// pages/_app.js
-// pages/_app.js
 import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
+import Cookies from 'js-cookie';
 
 function App({ Component, pageProps }) {
   const router = useRouter();
 
   useEffect(() => {
-    if (router.pathname !== '/disagree' && router.pathname !== '/privacy' && !localStorage.getItem('privacyPolicyAccepted')) {
+    const privacyPolicyAccepted = Cookies.get('privacyPolicyAccepted');
+
+    if (router.pathname !== '/disagree' && router.pathname !== '/privacy' && !privacyPolicyAccepted) {
       router.push('/privacy');
     }
   }, [router.pathname]);
