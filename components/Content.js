60046059afe94880f5b6a19a7c3d69e75db9ba7b
Sairam Prasanth Guthula <sg2366@njit.edu>
2023-05-08 05:42:55 -0400

    added components

---
+++ components/Content.js
@@ -0,0 +1,84 @@
+const Content = () => {
+  return (
+    <div class="bg-white py-24 sm:py-32">
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:text-center">
+          <h2 class="text-base font-semibold leading-7 text-indigo-600">
+            Explore more
+          </h2>
+          <p class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+            Everything you need to Learn at one stop.
+          </p>
+          <p class="mt-6 text-lg leading-8 text-gray-600"></p>
+        </div>
+        <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+          <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+            <div className="bg-violet-400 rounded-xl p-4">
+              <div class="relative pl-16">
+                <dt class="text-base font-serif font-bold leading-7 text-gray-900">
+                  AI-Driven Agile and Lean Methodologies: The Key to Success
+                </dt>
+                <dd class="mt-2 text-base leading-7 text-white">
+                  AI-driven Agile and Lean methodologies offer numerous benefits
+                  for schools, ranging from personalized learning experiences to
+                  streamlined administrative processes. By integrating these
+                  methodologies into your school's operations and learning
+                  environments, you can:
+                  <ul>
+                    <li>
+                      1. Deliver personalized learning experiences
+                    </li>
+                    <li>
+                      2. Enhance collaboration and communication
+                    </li>
+                    <li>
+                      3. Streamline administrative processes
+                    </li>
+                    <li>
+                      4. Make data-driven decisions
+                    </li>
+                    <li>
+                      5. Create a dynamic learning environment
+                    </li>
+                  </ul>
+                </dd>
+              </div>
+            </div>
+            <div className="bg-blue-400 rounded-xl p-4">
+              <div class="relative pl-16">
+                <dt class="text-base font-serif font-bold leading-7 text-gray-900">
+                  The Crucial Role of School Leaders in Driving Change
+                </dt>
+                <dd class="mt-2 text-base leading-7 text-white">
+                  As a school principal or administrator, you play a critical
+                  role in the successful adoption of AIdriven Agile and Lean
+                  methodologies in your institution. Here are some steps you can
+                  take to lead the transformation:
+                  <ul>
+                    <li>
+                      1. Develop a comprehensive understanding 
+                    </li>
+                    <li>
+                      2. Communicate the benefits and vision .
+                    </li>
+                    <li>
+                      3. Form strategic partnerships 
+                    </li>
+                    <li>
+                      4. Create a roadmap 
+                    </li>
+                    <li>
+                      5. Monitor progress and measure Success
+                    </li>
+                  </ul>
+                </dd>
+              </div>
+            </div>
+          </dl>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Content;
